976cde572fb15ee83f13be17877633feae0e82ff
Andrés Zamora <andres.zamora@correo.policia.gov.co>
2023-03-12 20:47:37 -0500

    Second commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
-const Card = () => {
+const Card = ({nombre, medico}) => {
+
+    const date = new Date()
+    const dia = date.getDate()
+    const mes = date.getMonth()
+
     return (
         <div>
-            Card
+             <h2>Los datos de su cita son:</h2>
+            <h3>Nombre: {nombre}</h3>           
+            <h3>Especialidad: {medico}</h3>
+            <h3>Fecha: {dia + 7}/{mes + 1} a las 16:00 horas</h3>
         </div>
     )
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,24 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from './Card'
 
 const Form = () => {
+    const[user, setUser] = useState({
+        nombre: '',
+        obraSocial: '',
+        email: '',
+        medico: ''
+    })
+
+    const [show, setShow] = useState(false)
+    const [err, setErr] = useState(false)
+
+    const handleSubmit = (event) =>{
+        event.preventDefault()
+        if(user.nombre.length > 8 && user.medico !== ''){
+            setShow(true)
+            setErr(false)
+        }else{
+            setShow(false)
+            setErr(true)
+        }
+    }
+
     return (
         <div>
-            <form>
-                <label htmlFor="">Nombres</label>
-                <input type="text" />
-                <label htmlFor="">Obra social</label>
-                <input type="text" />
-                <label htmlFor="">Email</label>
-                <input type="email" />
-                <select>
+            <form onSubmit={handleSubmit}>
+                <label>Nombres</label>
+                <input type="text" value={user.nombre} onChange={(e) => setUser({...user, nombre: e.target.value})}/>
+                <label>Obra social</label>
+                <input type="text" value={user.obraSocial} onChange={(e) => setUser({...user, obraSocial: e.target.value})}/>
+                <label>Email</label>
+                <input type="email" value={user.email} onChange={(e) => setUser({...user, email: e.target.value})}/>
+                <select value={user.medico} onChange={(e) => setUser({...user, medico: e.target.value})}>
                     <option value="">Seleccione una opción</option>
                     <option value="Dermatología">Dermatología</option>
                     <option value="Ortopedia">Ortopedia</option>
                     <option value="Cardiología">Cardiología</option>
                 </select>
+                <button>Enviar</button>
             </form>
-            <Card />
+            {err ? 'No ha ingresado la totalidad de la información' : null}
+            {show ? <Card nombre = {user.nombre} medico = {user.medico}/>: null}            
         </div>
     )
 }
